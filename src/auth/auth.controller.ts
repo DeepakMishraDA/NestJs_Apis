8c7c525eca7744014e9ea396f05c52d886d95336
deepak mishra <d.mishra@green-fusion.de>
2022-09-08 23:47:01 +0200

    get method added

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,12 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Get } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 
 @Controller({})
 export class AuthController {
     constructor(private authService: AuthService){}
+
+@Get()
+login(): void {
+    return this.authService.login();
+}
 }
